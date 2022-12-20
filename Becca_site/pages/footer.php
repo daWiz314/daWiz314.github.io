925524cf95813b14c1aecebeb130a4d0b42b04e4
Harry <programmer6299@icloud.com>
2022-12-20 10:17:28 -0700

    Adding footer for easier site management

---
+++ Becca_site/pages/footer.php
@@ -0,0 +1,13 @@
+<footer>
+        <div class="container">
+            <div id="contact_info">
+                <h5>Contact Info:</h5>
+                <ul>
+                    <li>Rebecca Martin</li>
+                    <li>91872398172389</li>
+                    <li>myEmail@Email.com</li>
+                </ul>
+            </div>
+            <p id="copy">You Are Awesome &copy;<? date('Y')?>. All rights reserved.</p>
+        </div>
+    </footer>

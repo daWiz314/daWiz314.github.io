38a197f67161c55b277111f28123045a8a494772
Harry <programmer6299@icloud.com>
2022-12-20 10:47:12 -0700

    Added link to nav bar

--- Becca_site/pages/floorPlanOne.php
+++ Becca_site/pages/floorPlanOne.php
@@ -24,30 +24,7 @@
 </head>
 
 <body data-bs-spy="scroll" data-bs-target="#navbarNav" data-bs-offset="0" class="scrollspy-example" tabindex="0">
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                Senior Project
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a href="../index.html" class="nav-link active" aria-current="page">Home</a></li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="stwo.html">Project Information</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="sthree.html">Designed Spaces</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="sfour.html">3D Renderings</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<?php include 'includes/nav.php'; ?>
     <main>
         <figure>
             <img src="../img/floor_plans/First%20Floor%20-%20Web%20Design.png" alt="">
--- Becca_site/pages/sfour.php
+++ Becca_site/pages/sfour.php
@@ -30,31 +30,7 @@
 <body data-bs-spy="scroll" data-bs-target="#navbarNav" data-bs-offset="0" class="scrollspy-example" tabindex="0">
 
   <!--Bootstrap Nav - https://getbootstrap.com/docs/5.0/components/navbar/#nav-->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            Senior Project
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a href="../index.html" class="nav-link">Home</a></li>
-                <li class="nav-item">
-                    <a class="nav-link" href="stwo.html">Project Information</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="sthree.html">Designed Spaces</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="sfour.html">3D Renderings</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+  <?php include 'includes/nav.php'; ?>
 
   <!-- Section Four -->
 

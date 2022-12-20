5b967c5070209e5637fec4ab1f55cba33070c290
Harry <programmer6299@icloud.com>
2022-12-20 10:27:07 -0700

    Created, moved nav bar to it

---
+++ Becca_site/pages/includes/nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                Senior Project
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="index.html" class="nav-link" aria-current="page">Home</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pages/stwo.html">Project Information</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pages/sthree.html">Designed Spaces</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pages/sfour.html">3D Renderings</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
